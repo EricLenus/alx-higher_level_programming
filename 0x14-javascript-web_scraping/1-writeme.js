785b095d9d5df7ce4d5009a41e2995a9b08e5063
EricLenus <ericledama@gmail.com>
2024-05-22 08:21:31 -0700

    writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
-  if (err) {
-    // Print the error object if an error occurred
-    console.error('Error:', err);
-    return;
-  }
-});
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
