8518802d4596ad600267e771e5a3fb2e82c16191
EricLenus <ericledama@gmail.com>
2024-05-22 08:23:10 -0700

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
+// Read from file
 
-const fs = require('fs');
-
-const filePath = process.argv[2];
-
-// Read the file asynchronously
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error:', err);
-  } else {
-    console.log('File content:', data);
-  }
-});
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
