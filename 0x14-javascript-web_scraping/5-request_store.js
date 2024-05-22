1ef507fe34e5c0e6eab5c024ee281214147f84cd
EricLenus <ericledama@gmail.com>
2024-05-22 08:29:34 -0700

    request store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
+// lorem ipsum
 
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request.get(url, (error, response, body) => {
+request.get(process.argv[2], (error, response, body) => {
   if (error) {
-    return;
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
   }
-  fs.writeFile(filePath, body, 'utf8', (err) => {
-    if (err) {
-      return;
-    }
-  });
 });
